0bc50239cf6c1ffd62eb9bf2f51176e44e684770
shko <work.shko@gmail.com>
2019-10-20 21:32:16 +0900

    SportsStore 따라만들기 진행중, ~ Page 212 1.빈 솔루션 생성 2. WebUI 프로젝트, Domain 프로젝트, 단위테스트 프로젝트 생성

---
+++ Sports/SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("403d7f11-83fb-404c-add4-8c88001b3fd4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
